44d432a450749e9bf7c90b3aea3c7defb8d2fbc9
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-11-10 06:56:53 -0300

    Bundle extension for json decoding

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Arthur Neves on 10/11/21.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+  let id: Int
+  let name: String
+}
