66caff8abed45fa09e0bdcb5b72aff8d2e201706
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-11-10 06:26:43 -0300

    Project setup

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Arthur Neves on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
