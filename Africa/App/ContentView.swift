79303cac286b536813b91acc66c0c4d47a7e36d3
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-11-11 06:54:07 -0300

    Animals list view created

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Content")
-            .padding()
+      
+      let animals: [Animal] = Bundle.main.decode("animals.json")
+        
+      NavigationView {
+        List {
+          CoverImageView()
+            .frame(height: 300)
+            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+          
+          ForEach(animals) { animal in
+            AnimalListItemView(animal: animal)
+              .listRowBackground(Color.black)
+          }
+        } //: List
+        .navigationBarTitle("Africa", displayMode: .large)
+      } //: NavigationView
     }
 }
 
