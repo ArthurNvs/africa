446a53224a9a16d3f169e38b0e43f8c12aa4df6b
Arthur Neves <arthurnvsm@gmail.com>
2021-11-16 09:17:21 -0300

    Gallery grid animation and haptics

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -12,6 +12,7 @@ struct GalleryView: View {
   @State private var selectedAnimal: String = "lion"
   
   let animals: [Animal] = Bundle.main.decode("animals.json")
+  let haptics = UIImpactFeedbackGenerator(style: .medium)
   
   // let gridLayout: [GridItem] = [
   //   GridItem(.flexible()),
@@ -54,9 +55,11 @@ struct GalleryView: View {
               .overlay(Circle().stroke(Color.white, lineWidth: 1))
               .onTapGesture {
                 selectedAnimal = item.image
+                haptics.impactOccurred()
               }
           } //: ForEach
         } //: LazyVGrid
+        .animation(.easeIn)
         .onAppear(perform: {
           gridSwitch()
         })
