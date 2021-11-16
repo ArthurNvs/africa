c11ab663ea4b04df3cf06d44ab38c0786ac826da
Arthur Neves <arthurnvsm@gmail.com>
2021-11-16 19:09:21 -0300

    GridView by button selection

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,30 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      
-      let animals: [Animal] = Bundle.main.decode("animals.json")
-        
-      NavigationView {
-        List {
-          CoverImageView()
-            .frame(height: 300)
-            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-          
-          ForEach(animals) { animal in
-            NavigationLink(destination: AnimalDetailView(animal: animal)) {
-              AnimalListItemView(animal: animal)
-            }
-            .listRowBackground(Color.black)
+  let animals: [Animal] = Bundle.main.decode("animals.json")
+  let haptics = UIImpactFeedbackGenerator(style: .medium)
+  
+  @State private var isGridViewActive = false
+  
+  let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+  
+  var body: some View {
+    
+    NavigationView {
+      Group {
+        if !isGridViewActive {
+          List {
+            CoverImageView()
+              .frame(height: 300)
+              .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            
+            ForEach(animals) { animal in
+              NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                AnimalListItemView(animal: animal)
+              }
+              .listRowBackground(Color.black)
+            } //: ForEach
           }
+        } else {
+          ScrollView(.vertical, showsIndicators: false) {
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+              ForEach(animals) { animal in
+                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                  AnimalGridItemView(animal: animal)
+                }
+              } //: ForEach
+            } //: LazyVGrid
+            .padding(10)
+            .animation(.easeIn)
+          } //: ScrollView
         } //: List
-        .navigationBarTitle("Africa", displayMode: .large)
-      } //: NavigationView
-    }
+      } //: Group
+      .navigationBarTitle("Africa", displayMode: .large)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          HStack(spacing: 16) {
+            // LIST
+            Button(action: {
+              print("List view is activated")
+              isGridViewActive = false
+              haptics.impactOccurred()
+            }) {
+              Image(systemName: "square.fill.text.grid.1x2")
+                .font(.title2)
+                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+            }
+            
+            // GRID
+            Button(action: {
+              print("Grid view is activated")
+              isGridViewActive = true
+              haptics.impactOccurred()
+            }) {
+              Image(systemName: "square.grid.2x2")
+                .font(.title2)
+                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+            }
+          } //: HStack
+        } //: Buttons/ToolbarItem
+      } //: Toolbar
+    } //: NavigationView
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
